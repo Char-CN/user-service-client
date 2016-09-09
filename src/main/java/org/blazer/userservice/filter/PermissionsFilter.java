9e333572197c3b5d90162228e2dc33786942b8e8
hyy <hyy@192.168.112.1>
2016-09-09 18:02:56 +0800

    commit

--- src/main/java/org/blazer/userservice/filter/PermissionsFilter.java
+++ src/main/java/org/blazer/userservice/filter/PermissionsFilter.java
@@ -10,7 +10,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -28,6 +30,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PermissionsFilter implements Filter {
 
+	private static final String SESSION_KEY = "MYSESSIONID";
 	private static ThreadLocal<HttpServletRequest> HttpRequestThreadLocalHolder = new ThreadLocal<HttpServletRequest>(); 
 	private String systemName = null;
 	private String serviceUrl = null;
@@ -51,7 +54,7 @@ public class PermissionsFilter implements Filter {
 		} finally {
 			if (in != null) {
 				try {
-					in.close();// 最后要关闭BufferedReader
+					in.close();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -61,22 +64,45 @@ public class PermissionsFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-		if (systemName != null) {
-			HttpServletRequest request = (HttpServletRequest) req;
-			System.out.println(request.getRequestURL());
-			System.out.println(request.getRequestURI());
-			System.out.println(request.getRemoteHost());
-			System.out.println(request.getRemoteAddr());
-			try {
-				String content = executeGet(serviceUrl + "/getuser.do");
-				ObjectMapper mapper = new ObjectMapper();
-				UserModel um = mapper.readValue(content, UserModel.class);
-				System.out.println(um);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		HttpServletRequest request = (HttpServletRequest) req;
+		String sessionid = getSessionId(request);
+		System.out.println(request.getRequestURL());
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getRemoteHost());
+		System.out.println(request.getRemoteAddr());
+		String url = request.getRequestURI();
+		try {
+//			String content = executeGet(serviceUrl + "/getuser.do?" + SESSION_KEY + "=" + sessionid);
+//			ObjectMapper mapper = new ObjectMapper();
+//			UserModel um = mapper.readValue(content, UserModel.class);
+//			System.out.println(um);
+			StringBuilder requestUrl = new StringBuilder(serviceUrl);
+			requestUrl.append("/checkurl.do?");
+			requestUrl.append(SESSION_KEY).append("=").append(sessionid);
+			requestUrl.append("&").append("url").append("=").append(url);
+			requestUrl.append("&").append("systemName").append("=").append(systemName);
+			String content = executeGet(requestUrl.toString());
+			System.out.println(content);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		chain.doFilter(req, resp);
+	}
+
+	private String getSessionId(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies == null) {
+			return null;
+		}
+		Cookie sessionCookie = null;
+		for (Cookie cookie : cookies) {
+			if (SESSION_KEY.equals(cookie.getName())) {
+				System.out.println("cookie : " + cookie.getName() + " | " + cookie.getValue());
+				sessionCookie = cookie;
+				break;
+			}
+		}
+		return sessionCookie == null ? null : sessionCookie.getValue();
 	}
 
 	public void init(FilterConfig filterConfig) throws ServletException {
